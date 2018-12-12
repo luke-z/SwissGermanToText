35219c066a7af46b51f5c74162a867d58a65568d
Lukas Zbinden <lukas.zbinden@students.bfh.ch>
2018-12-12 15:46:02 +0100

    changes to export summary

--- export_model.py
+++ export_model.py
@@ -10,10 +10,14 @@ def export_model(model, settings):
 
     settings_file = open("./models/" + foldername + "/model_settings.txt", "w+")
     for key,val in settings.items():
-        settings_file.write(str(key) + ": " + str(val) + "\n")
+        settings_file.write(str(key) + ":\n" + str(val) + "\n")
+    settings_file.write("Model summary:\n")
+    model.summary(print_fn=lambda x: settings_file.write(x + '\n'))
     settings_file.close()
 
     model.save("./models/" + foldername + "/SwissGermanToText.model")
+
+    return "Exported to folder ./models/" + foldername
 
 
 def import_model(path):
